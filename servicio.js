afb59742ed8f84b225cd6c23af91ad6546b7e546
adny osorio <huertaslina61@gmail.com>
2022-03-03 15:17:45 -0500

    se realizaron cambios en el index y en servicios agregando codigos

--- servicio.js
+++ servicio.js
@@ -1,7 +1,7 @@
 // consumo(traer datos) de appi desde un cliente js
 
 const URI="https://api.spotify.com/v1/artists/4N5fp4zhTsVITZTVfsXpc2/top-tracks?market=US" 
-const TOKEN= "Bearer BQCRq7Vef7nFodFqu-SlAh3vDhGODKMKTG9DjYbCyNCufNKd3gqvlN3O7eA-t68Citi-jL5J7mDKd_pJ2wKedzfD2jWYw2ImgJzyBlANpez07ZH5VFnr-26SWVt27soeL5uk_6pOfCbNdQFHJCJ6levF9noDjdE"
+const TOKEN= "Bearer BQAmDlH4a36FdWpZHds0Kjr88K9B58CYo57v6n_r3io5HEScqkQkqjhRRoq-aAqRpE3PSBglPF7r93-RtmfiRVsEojjkg2gM4ejO08X-CO1do6cq6O0ZkvYBQaRlnyAW34N03um9X_3tlSEEO4KNbw8V_ptm81s"
 
 // los parametros siempre seran un objeto
 //1. method
@@ -19,7 +19,59 @@ fetch(URI,PARAMETROS_PETICION)
 })
 .then(function(respuesta){ //hago lo que necesite con la respuesta
     pintarCanciones(respuesta.tracks)
+    let fila= document.getElementById("fila")
+    
+    respuesta.tracks.forEach(function (cancion){  
+       /* console.log(cancion.name) 
+        console.log(cancion.preview_url) 
+        console.log(cancion.album.images[0].url)*/
+
+        let columna=document.createElement("div")
+        //a columna le agregagamos la clase col
+        columna.classList.add("col")
+    
+        let tarjeta = document.createElement("div")
+        tarjeta.classList.add("card")
+        tarjeta.classList.add("h-100")
+
+        let imagen = document.createElement("img")
+        imagen.classList.add("card-img-top")
+        imagen.classList.add("h-100")
+        imagen.classList.add("p-3")
+        imagen.src=cancion.album.images[0].url
+
+        let separador = document.createElement("hr")
+        separador.classList.add("w-50")
+        separador.classList.add("mx-auto")
+        separador.classList.add("d-block")
+
+        let cuadroTexto = document.createElement("div")
+        cuadroTexto.classList.add("px-3")
+        cuadroTexto.classList.add("text-center")
+    
+        let nombreCancion = document.createElement("h4")
+        nombreCancion.textContent=cancion.name
+
+        let direccionCancion = document.createElement("audio")
+        direccionCancion.src=cancion.preview_url
+        direccionCancion.setAttribute("controls","controls")
+
+
+        cuadroTexto.appendChild(nombreCancion)
+        cuadroTexto.appendChild(direccionCancion)
+    
+        tarjeta.appendChild(imagen)
+        tarjeta.appendChild(separador)
+        tarjeta.appendChild(cuadroTexto)
+        
+        columna.appendChild(tarjeta)
+        fila.appendChild(columna)
+
+
+
+    }) 
 })
+
 .catch(function(respuesta){ // mostrar el mensaje de error  de la respuesta
     console.log("UPS... "+ respuesta)
 })
